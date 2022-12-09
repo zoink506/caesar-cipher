fb81a406aeeba2b5633ff10ebb299e8495f53ca0
zoink506 <benhubble03@gmail.com>
2022-12-09 22:20:36 +1100

    Detect when char's ascii value is within range of alphabet

--- caesar.rb
+++ caesar.rb
@@ -6,20 +6,26 @@ def getInput()
   amount = gets.chomp()
 
   result = calculate_cipher(input, amount)
-  p result
 end 
 
 def calculate_cipher(input = "", amount)
-  # First, check to see if input and amount are valid strings and numbers
-  # If not a valid string/number, return nil
+  # Check to see if input and amount are valid strings and numbers
+  return :error if amount.to_i == 0 || input.ascii_only? == false
+
   # Convert input into array of ascii values
+  charArray = input.bytes
+
   # If the ascii value is within range of a-z/A-Z, add amount to it to shift its value
   # If new ascii value is now outside range of a-z/A-Z, minus 26 to loop around
-  return :error if amount.to_i == 0 || input.ascii_only? == false
-  
-  
+  charArray.each do |char|
+    if (char >= 65 && char <= 90) || (char >= 97 && char <= 122)
+      puts "#{char.chr} - #{char}: within range"
+    else 
+      puts "#{char.chr} - #{char} outside range"
+    end 
+  end 
 
-  input
+  charArray
 end 
 
 getInput()
