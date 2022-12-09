ae45c430bd7fadfd7e03c6bd1442fc6bc2a7a510
zoink506 <benhubble03@gmail.com>
2022-12-09 23:36:29 +1100

    Characters loop around the alphabet if increment is higher than 26

--- caesar.rb
+++ caesar.rb
@@ -3,29 +3,52 @@ def getInput()
   puts "Type the sentence you wish to encrypt: "
   input = gets.chomp
   puts "Type the amount of characters to shift: "
-  amount = gets.chomp()
+  amount = gets.chomp
 
   result = calculate_cipher(input, amount)
+  #puts "\n"
+  result.each { |char| print char.chr }
+  print "\n"
 end 
 
 def calculate_cipher(input = "", amount)
   # Check to see if input and amount are valid strings and numbers
-  return :error if amount.to_i == 0 || input.ascii_only? == false
+  return :error if amount.to_i == 0 || input.ascii_only? == false || input == ""
+  amount = amount.to_i
 
   # Convert input into array of ascii values
-  charArray = input.bytes
-
+  char_array = input.bytes
+  p char_array
+  
   # If the ascii value is within range of a-z/A-Z, add amount to it to shift its value
-  # If new ascii value is now outside range of a-z/A-Z, minus 26 to loop around
-  charArray.each do |char|
-    if (char >= 65 && char <= 90) || (char >= 97 && char <= 122)
-      puts "#{char.chr} - #{char}: within range"
+   new_char_array = char_array.map do |char|
+    if char >= 65 && char <= 90
+      # UPPERCASE
+      char += amount
+
+      # How to find the amount of times to loop before actually looping?
+      # Inefficient code loops multiple times when looping isn't necessary
+      while char > 90
+        char -= 26
+      end 
+      char # return char to map method
+
+    elsif char >= 97 && char <= 122
+      # lowercase
+      char += amount
+
+      while char > 122
+        char -= 26
+      end 
+      char # return char to map method
+
     else 
-      puts "#{char.chr} - #{char} outside range"
+      char
     end 
   end 
 
-  charArray
+  p new_char_array
+  new_char_array
 end 
 
 getInput()
