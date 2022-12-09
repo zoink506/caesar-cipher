bff6a8b15d0e21d9b812843ee68cb54df0768a28
zoink506 <benhubble03@gmail.com>
2022-12-09 21:46:12 +1100

    Add input validation

--- caesar.rb
+++ caesar.rb
@@ -1,14 +1,25 @@
-def getInput
+def getInput()
+  # Gets input from the user, then supplies that information to the encryption function
   puts "Type the sentence you wish to encrypt: "
   input = gets.chomp
   puts "Type the amount of characters to shift: "
   amount = gets.chomp()
 
-  calculate_cipher(input, amount)
+  result = calculate_cipher(input, amount)
+  p result
 end 
 
 def calculate_cipher(input = "", amount)
+  # First, check to see if input and amount are valid strings and numbers
+  # If not a valid string/number, return nil
+  # Convert input into array of ascii values
+  # If the ascii value is within range of a-z/A-Z, add amount to it to shift its value
+  # If new ascii value is now outside range of a-z/A-Z, minus 26 to loop around
+  return :error if amount.to_i == 0 || input.ascii_only? == false
+  
+  
 
+  input
 end 
 
 getInput()
