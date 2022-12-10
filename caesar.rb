4ab37135d27d3820f1a19b0616f4a5c9ed658674
zoink506 <benhubble03@gmail.com>
2022-12-10 11:28:10 +1100

    Add looping functionality if amount is higher than 26

--- caesar.rb
+++ caesar.rb
@@ -6,19 +6,17 @@ def getInput()
   amount = gets.chomp
 
   result = calculate_cipher(input, amount)
-  #puts "\n"
-  result.each { |char| print char.chr }
-  print "\n"
+  puts "\n"
+  p result
 end 
 
 def calculate_cipher(input = "", amount)
   # Check to see if input and amount are valid strings and numbers
-  return :error if amount.to_i == 0 || input.ascii_only? == false || input == ""
+  return :error if amount.to_i <= 0 || input.ascii_only? == false || input == ""
   amount = amount.to_i
 
   # Convert input into array of ascii values
   char_array = input.bytes
-  p char_array
   
   # If the ascii value is within range of a-z/A-Z, add amount to it to shift its value
    new_char_array = char_array.map do |char|
@@ -26,29 +24,32 @@ def calculate_cipher(input = "", amount)
       # UPPERCASE
       char += amount
 
-      # How to find the amount of times to loop before actually looping?
-      # Inefficient code loops multiple times when looping isn't necessary
-      while char > 90
-        char -= 26
+      if char > 90
+        char -= (((char - 65) / 26).floor) * 26
       end 
-      char # return char to map method
 
+      char # return char to map method
     elsif char >= 97 && char <= 122
-      # lowercase
+      # LOWERCASE
       char += amount
 
-      while char > 122
-        char -= 26
-      end 
-      char # return char to map method
+      # Checks if the result of adding the specified amount is greater than 122 (ascii value for z, last lowercase letter)
+      # If not, then continue without an action (ascii value is within range of lowercase letters)
+      # If so, keep minusing 26 (length of alphabet) from char until it is within range
+      # Equation finds how many times to minus 26, then * by 26 to minus in one go instead of using a loop (would be inefficient)
+      if char > 122
+        char -= (((char - 97) / 26).floor) * 26
+      end
 
+      char # return char to map method
     else 
+      # char is not a letter, so return it unchanged
       char
     end 
   end 
 
-  p new_char_array
-  new_char_array
+  new_char_array.map! { |char| char.chr }
+  new_char_array.join("")
 end 
 
 getInput()
