45bd87bc8cf1b9180a853bee505c733c6ce1b76e
zoink506 <benhubble03@gmail.com>
2023-01-04 15:41:12 +1100

    Finish writing tests

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -2,10 +2,6 @@ require './caesar.rb'
 
 describe 'Caesar Cipher program' do
   describe 'caesar_cipher method' do
-    it "returns 'ifmmp xpsme' for 'hello world', amount 1" do
-      expect(calculate_cipher('hello world', 1)).to eql('ifmmp xpsme')
-    end
-
     it "returns nil if an empty string is provided" do
       expect(calculate_cipher('', 1)).to eql(nil)
     end
@@ -15,8 +11,32 @@ describe 'Caesar Cipher program' do
       expect(calculate_cipher(574, 1)).to eql(nil)
     end
 
+    it "returns nil if any non-ascii characters are present" do
+      expect(calculate_cipher('el➙', 1)).to eql(nil)
+    end
+
+    it "returns 'ifmmp xpsme' for 'hello world', amount 1" do
+      expect(calculate_cipher('hello world', 1)).to eql('ifmmp xpsme')
+    end
+
     it "returns 'olssv dvysk' for 'hello world', amount 7" do
       expect(calculate_cipher('hello world', 7)).to eql('olssv dvysk')
+    end
+
+    it "returns 'ifmmp xpsme' for 'hello world', amount 27" do
+      expect(calculate_cipher('hello world', 27)).to eql('ifmmp xpsme')
+    end
+
+    it "returns 'yvccf nficu' for 'hello world', amount 95" do
+      expect(calculate_cipher('hello world', 95)).to eql('yvccf nficu')
+    end
+
+    it "Maintains capital numbers" do
+      expect(calculate_cipher('HelLo WoRLd', 1)).to eql('IfmMp XpSMe')
+    end
+
+    it "Ignores punctuation marks" do
+      expect(calculate_cipher("[:hello, (world)!]", 1)).to eql("[:ifmmp, (xpsme)!]")
     end
   end
 end
